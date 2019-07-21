84240f09bfbf7a99d8cae6a5fe14491f4d488322
Tony Cruz <tonycruz417@gmail.com>
2019-07-21 14:59:53 -0600

    working on click events

--- pic-game/src/components/Pictures.js
+++ pic-game/src/components/Pictures.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import mclaren from '../images/senna.jpg';
 import blueMclaren from '../images/blueSenna.jpg';
+import orangeMclaren from '../images/mclarenOrange.jpg';
 
 const Display = () => {
 
+    var picSelect = () => {
+        console.log(`You clicked an image`);
+    }
 
     return(
-        <div>
-            <img src={mclaren} width="300px" alt="A Mclaren Senna" />
-            <img src={blueMclaren} width="240px" alt="A Mclaren Senna" />
+        <div className="picRow">
+            <img src={mclaren} width="300px" alt="A Mclaren Senna" onClick={picSelect} />
+            <img src={blueMclaren} width="240px" alt="A Mclaren Senna" onClick={picSelect}/>
+            <img src={orangeMclaren} width="270px" alt="An orange Mclaren Senaa" onClick={picSelect}/>
         
         </div>
     )
