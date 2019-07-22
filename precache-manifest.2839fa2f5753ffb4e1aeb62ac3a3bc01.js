d4b57a0e893388fbcc2bef3b55fc6c5b6d3b1e52
unknown <tonycruz417@gmail.com>
2019-07-22 18:14:34 -0500

    Updates

---
+++ precache-manifest.2839fa2f5753ffb4e1aeb62ac3a3bc01.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db6bb048382c7ad8af72ae8cae75110c",
+    "url": "/Memory-Game/index.html"
+  },
+  {
+    "revision": "70023600425acb5b06aa",
+    "url": "/Memory-Game/static/css/main.c8e81b74.chunk.css"
+  },
+  {
+    "revision": "503eb234aa57c09220e5",
+    "url": "/Memory-Game/static/js/2.2d964e69.chunk.js"
+  },
+  {
+    "revision": "70023600425acb5b06aa",
+    "url": "/Memory-Game/static/js/main.89b27e0f.chunk.js"
+  },
+  {
+    "revision": "8d83c22dcc2611df7753",
+    "url": "/Memory-Game/static/js/runtime~main.81d8dd5c.js"
+  },
+  {
+    "revision": "98cb813a7432f5e18c88ad5d4e2482b0",
+    "url": "/Memory-Game/static/media/blueSenna.98cb813a.jpg"
+  },
+  {
+    "revision": "e289067788e3972ad30d87f773daafbc",
+    "url": "/Memory-Game/static/media/mclarenOrange.e2890677.jpg"
+  },
+  {
+    "revision": "e78faf08a9fd9e6153686b7b380027c5",
+    "url": "/Memory-Game/static/media/senna.e78faf08.jpg"
+  }
+]);
